46081fce8f5aecf9bec5be2dc3d36fb1c4bad595
MacarenaCarrero <macarena.c.rios@gmail.com>
2024-12-11 23:26:18 +0100

    Rewsults Summary

--- src/components/parametros/Parametros.jsx
+++ src/components/parametros/Parametros.jsx
@@ -1,10 +1,29 @@
+import { SUMMARY_INFO } from '../../constants/summary-info';
 import styles from './parametros.module.css';
 
 const Parametros = ({}) => {
 	const parametrosStyle = styles.parametros;
-	return <div className={parametrosStyle}>
-		
-	</div>;
-};
+	const senseStyle = styles.sense;
+	
+	
+
+	return (
+		<>
+		{SUMMARY_INFO.map (parametros => {
+			return (
+				<div key={SUMMARY_INFO.id}>
+				</div>
+			)
+		}) }
+		</>
+	)
+	//return (
+	//	<>
+	//<div className={parametrosStyle}>
+		//<img src="" alt="" />
+		//<span className={senseStyle}></span>
+	//</div>);
+	//</>
+//};
 
 export default Parametros;
--- src/constants/summary-info.js
+++ src/constants/summary-info.js
@@ -1,5 +1,33 @@
-export const SUMMARY_INFO =[
-    {
-        
-    }
-]
+import { v4 } from 'uuid';
+
+export const SUMMARY_INFO = [
+	{
+		//pasamos la info como un objeto
+		id: v4(),
+		icono: 'imagen',
+		title: 'title',
+		num1: '23',
+		num2: '/56'
+	},
+	{
+		id: v4(),
+		icono: 'imagen',
+		title: 'title',
+		num1: '23',
+		num2: '/56'
+	},
+	{
+		id: v4(),
+		icono: 'imagen',
+		title: 'title',
+		num1: '23',
+		num2: '/56'
+	},
+	{
+		id: v4(),
+		icono: 'imagen',
+		title: 'title',
+		num1: '23',
+		num2: '/56'
+	}
+];
